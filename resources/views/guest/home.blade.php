9763dac6f39eba7c55804c9cae33861586bc0e07
mattia laurenzana <mattilaure@gmail.com>
2022-03-14 00:10:47 +0100

    Exercise completed

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    <div id="app">
+        <ul>
+            <li v-for="i in 10" v-html="i">
+
+            </li>
+        </ul>
+    </div>
+
+    <script src="{{asset('js/vue.js')}}"></script>
+</body>
+</html>
